d507326c3321a76da4c092d40275d1da51dbf1fd
Souk2323 <elsouk04@gmail.com>
2026-04-26 21:07:42 +0200

    Rename variable 'texte' to 'texteBase' for clarity

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,20 @@
-let texte = "";
+let texteBase = "";
 
-// Charger fichier
 document.getElementById('fileInput').onchange = function(e) {
     let reader = new FileReader();
     reader.onload = function() {
-        texte = reader.result;
-        document.getElementById('affichageGauche').value = texte;
+        texteBase = reader.result;
+        document.getElementById('affichageGauche').value = texteBase;
     };
     reader.readAsText(e.target.files[0]);
 };
 
-// Bonjour
 document.getElementById('btnBonjour').onclick = () => alert("Bonjour Soukaina !");
 
-// Segmentation (Consigne Page 13)
 document.getElementById('btnSegmentation').onclick = () => {
-    let d = document.getElementById('delims').value;
-    let re = new RegExp(d, "g");
-    let mots = texte.split(re).filter(m => m.length > 0);
+    let delim = document.getElementById('delims').value;
+    let regex = new RegExp(delim, "g");
+    let mots = texteBase.split(regex).filter(m => m.length > 0);
     document.getElementById('affichageDroit').innerText = mots.join(" | ");
     document.getElementById('stats').innerText = "Nombre de formes : " + mots.length;
 };
